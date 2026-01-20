bafea97c2963e78546cd1f9d6e90f476619100e3
ulugbek <udavlatboyev@gmail.com>
2026-01-20 19:04:20 +0500

    urls

---
+++ configapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'accounts', views.AccountViewSet, basename='account')
+router.register(r'transactions', views.TransactionViewSet, basename='transaction')
+router.register(r'goals', views.GoalViewSet, basename='goal')
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('api/', include(router.urls)),
+]
--- configapp/views.py
+++ configapp/views.py
@@ -1,20 +1,45 @@
-from django.shortcuts import render
-from .models import Account, Currency
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from rest_framework import viewsets
+from .models import User, Account, Transaction, FinancialGoal, Currency
+from .serializers import AccountSerializer, TransactionSerializer, GoalSerializer
 
 
 def dashboard(request):
-    user_accounts = Account.objects.filter(user=request.user)
     selected_code = request.GET.get('currency', 'UZS')
     target_currency = Currency.objects.get(code=selected_code)
+    accounts = Account.objects.filter(user=request.user)
 
     total_balance = 0
-    for acc in user_accounts:
-        in_base = acc.balance * acc.currency.rate
-        converted = in_base / target_currency.rate
+    for acc in accounts:
+        balance_in_base = acc.balance * acc.currency.rate
+        converted = balance_in_base / target_currency.rate
         total_balance += converted
 
     return render(request, 'dashboard.html', {
-        'accounts': user_accounts,
+        'accounts': accounts,
         'total': total_balance,
-        'currency': target_currency
+        'selected_currency': target_currency,
+        'all_currencies': Currency.objects.all()
     })
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    serializer_class = AccountSerializer
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        return Transaction.objects.filter(account__user=self.request.user)
+
+
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+
+    def get_queryset(self):
+        return FinancialGoal.objects.filter(user=self.request.user)
