ae2dcf145622aa7e56ee040953b58518e7e7e7eb
ulugbek <udavlatboyev@gmail.com>
2026-01-20 19:23:05 +0500

    template

--- configapp/views.py
+++ configapp/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, redirect
-from django.utils import timezone
-from rest_framework import viewsets
-from .models import User, Account, Transaction, FinancialGoal, Currency
-from .serializers import AccountSerializer, TransactionSerializer, GoalSerializer
+from django.shortcuts import render
+from django.db.models import Sum
+from .models import Account, Currency, Transaction, FinancialGoal
 
 
 def dashboard(request):
     selected_code = request.GET.get('currency', 'UZS')
     target_currency = Currency.objects.get(code=selected_code)
     accounts = Account.objects.filter(user=request.user)
+    goals = FinancialGoal.objects.filter(user=request.user)
 
     total_balance = 0
     for acc in accounts:
@@ -16,30 +15,20 @@ def dashboard(request):
         converted = balance_in_base / target_currency.rate
         total_balance += converted
 
+    expenses = Transaction.objects.filter(
+        account__user=request.user,
+        type='EXPENSE'
+    ).values('category').annotate(total=Sum('amount'))
+
+    chart_labels = [item['category'] for item in expenses]
+    chart_data = [float(item['total']) for item in expenses]
+
     return render(request, 'dashboard.html', {
         'accounts': accounts,
+        'goals': goals,
         'total': total_balance,
         'selected_currency': target_currency,
-        'all_currencies': Currency.objects.all()
+        'all_currencies': Currency.objects.all(),
+        'chart_labels': chart_labels,
+        'chart_data': chart_data,
     })
-
-
-class AccountViewSet(viewsets.ModelViewSet):
-    serializer_class = AccountSerializer
-
-    def get_queryset(self):
-        return Account.objects.filter(user=self.request.user)
-
-
-class TransactionViewSet(viewsets.ModelViewSet):
-    serializer_class = TransactionSerializer
-
-    def get_queryset(self):
-        return Transaction.objects.filter(account__user=self.request.user)
-
-
-class GoalViewSet(viewsets.ModelViewSet):
-    serializer_class = GoalSerializer
-
-    def get_queryset(self):
-        return FinancialGoal.objects.filter(user=self.request.user)
